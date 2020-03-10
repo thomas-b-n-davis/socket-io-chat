02115495e88f29b41d530c41bf9499e9d179aca6
thomas davis <thomasdavis@Sebastians-iMac.fritz.box>
2020-03-10 14:30:17 +0100

    socket IO chat

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ let express =require("express");
 let app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
-let port =3000;
+let port =3010;
 
-io.on("connection", socket => {
-  console.log("a user connected :D");
-  socket.on("chat message", msg => {
-    console.log(msg);
-    io.emit("chat message", msg);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat',function(msg){
+    console.log('message: ' + msg);
+    let message=msg.split("::");
+    io.emit(message[0], message[1]);
   });
 });
 
+
 app.get('/',(req,res)=>{
-  console.log("---->");
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(port, () => console.log("server running on port:" + port));
+
+
